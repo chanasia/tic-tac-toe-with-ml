6ee35c09a993a9298d191b57f7db423a0ade02c2
Chananuwat Chaysri <asiavip2546@gmail.com>
2022-09-27 21:50:02 +0000

    drag and drop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+</style>
+
+<script lang="ts">
+    import Draggable from "./Draggable.svelte";
+</script>
+
+<Draggable>
+    <h1>
+        Click me
+    </h1>
+</Draggable>
---
+++ src/routes/Draggable.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let left: Number = 30
+    export let top: Number = 30
+    let moving:boolean = false
+
+    function start(){
+        moving = true
+    }
+
+    function stop(){
+        moving = false
+    }
+
+    function move(e:any){
+        if(moving){
+            left += e.movementX
+            top += e.movementY
+        }
+    }
+</script>
+
+<style>
+    .draggable{
+        user-select: none;
+        position:absolute;
+        border: solid 1px gray;
+        cursor: grab;
+    }
+</style>
+
+<svelte:window on:mouseup={stop} on:mousemove={move}/>
+
+<section on:mousedown={start} style="left: {left}px; top: {top}px;" class="draggable">
+    <slot></slot>
+</section>
